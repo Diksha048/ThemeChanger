b87690444237878e1ebf3a830f0fd12b502641ba
Diksha048 <bhagwatdiksha0408@gmail.com>
2024-07-02 19:04:58 +0530

    dark theme function added

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-function calculateBMI(){
+function calculateBMI()
+{
     const form = document.getElementById("box-container");
-const weightinput = document.getElementById("weight");
-const heightinput = document.getElementById("height");
-const resultdiv = document.getElementById("result");
+    const weightinput = document.getElementById("weight");
+    const heightinput = document.getElementById("height");
+    const resultdiv = document.getElementById("result");
 
-const weight = parseFloat(weightinput.value);
-const height = parseFloat(heightinput.value);
+    const weight = parseFloat(weightinput.value);
+    const height = parseFloat(heightinput.value);
 
 if(weight && height)
 {
@@ -19,13 +20,15 @@ else
 }
 }
 
-function calculateBMIValue(weight,height){
+function calculateBMIValue(weight,height)
+{
     const heightsquare = height * height;
     const bmi = weight / heightsquare *10000;
     return Math.round(bmi * 100) / 100;
 }
 
-function categorizeBMI(bmi){
+function categorizeBMI(bmi)
+{
     if(bmi < 18.5)
     {
         return "Under weight";
@@ -42,4 +45,29 @@ function categorizeBMI(bmi){
     {
         return "Obese";
     }
+}
+/* Theme Slector code */
+
+function enabledark()
+{
+    const mainBody = document.getElementById("main-body");
+    mainBody.classList.add("bg-dark");
+    mainBody.classList.remove("bg-light");
+
+    const Boxcontainer = document.getElementById("box-container");
+    Boxcontainer.classList.add("box-container-dark");
+
+    const buttoncontainer = document.getElementById("button");
+    Boxcontainer.classList.add("button-bg");
+
+    const resultbg = document.getElementById("result");
+    resultbg.classList.add("result-bg");
+
+}
+
+function enablelight()
+{
+    const mainBody = document.getElementById("main-body");
+    mainBody.classList.add("bg-light");
+    mainBody.classList.remove("bg-dark");
 }
